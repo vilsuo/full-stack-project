d83a94451da81b0bca5863978661103ae175721a
Ville Suokas <vilsuo1@gmail.com>
2023-10-31 23:11:13 +0200

    added dependecies for testing setup

--- backend/src/util/config.js
+++ backend/src/util/config.js
@@ -2,7 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   SECRET: process.env.SECRET,
-  DATABASE_URL: process.env.DATABASE_URL,
-  REDIS_URL: process.env.REDIS_URL,
+  DATABASE_URL: process.env.NODE_ENV === 'test'
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
+  REDIS_URL: process.env.NODE_ENV === 'test'
+    ? process.env.TEST_REDIS_URL
+    : process.env.REDIS_URL,
   PORT: process.env.PORT || 3001
 };
