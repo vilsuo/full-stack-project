c6557b8b4788ec5605502828a9e474757a674af0
Ville Suokas <vilsuo1@gmail.com>
2023-11-01 21:31:25 +0200

    moved app to its own file

---
+++ backend/src/app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+require('express-async-errors');
+
+const cors = require('cors');
+
+const { SECRET } = require('./util/config');
+
+const { redisClient } = require('./util/db');
+
+const authRouter = require('./controllers/auth');
+const usersRouter = require('./controllers/users');
+const testRouter = require('./controllers/testr');
+
+const { requestLogger, errorHandler, unknownEndpoint } = require('./util/middleware');
+
+const session = require('express-session');
+const RedisStore = require('connect-redis').default;
+
+
+const app = express();
+
+// MIDDLEWARE
+app.use(express.json());
+
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
+  credentials: true,
+}));
+
+app.use(requestLogger);
+
+app.use(session({
+  store: new RedisStore({ client: redisClient }),
+  resave: false,
+  saveUninitialized: false,
+  secret: SECRET,
+  cookie: {
+    // if true only transmit cookie over https
+    secure: process.env.NODE_ENV === "production",
+
+    // if true prevent client side JS from reading cookie
+    httpOnly: true,
+  },
+}));
+
+// ROUTES
+app.use('/api/auth', authRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/test', testRouter);
+
+// OTHER MIDDLEWARE
+app.use(unknownEndpoint);
+
+app.use(errorHandler);
+
+module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,59 +1,13 @@
-const express = require('express');
-require('express-async-errors');
-
-const cors = require('cors');
-
-const { PORT, SECRET } = require('./util/config');
-
-const { redisClient, connectToDatabases } = require('./util/db');
-
-const authRouter = require('./controllers/auth');
-const usersRouter = require('./controllers/users');
-const testRouter = require('./controllers/test');
-
-const { requestLogger, errorHandler, unknownEndpoint } = require('./util/middleware');
-
-const session = require('express-session');
-const RedisStore = require('connect-redis').default;
-
-const app = express();
-
-app.use(express.json());
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
-  methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
-  credentials: true,
-}));
-
-app.use(requestLogger);
-
-app.use(session({
-  store: new RedisStore({ client: redisClient }),
-  resave: false,
-  saveUninitialized: false,
-  secret: SECRET,
-  cookie: {
-    // if true only transmit cookie over https
-    secure: process.env.NODE_ENV === "production",
-
-    // if true prevent client side JS from reading cookie
-    httpOnly: true,
-  },
-}));
-
-app.use('/api/auth', authRouter);
-app.use('/api/users', usersRouter);
-app.use('/api/test', testRouter);
-
-app.use(unknownEndpoint);
-
-app.use(errorHandler);
+const app = require('./app');
+const { PORT } = require('./util/config');
+const { connectToDatabases } = require('./util/db');
+const logger = require('./util/logger');
 
 const start = async () => {
   await connectToDatabases();
 
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    logger.info(`Server running on port ${PORT}`);
   });
 }
 
