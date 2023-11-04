205c867cf2c094efbd153786969d2664053f1b55
Ville Suokas <vilsuo1@gmail.com>
2023-11-04 12:57:44 +0200

    added querying for /api/users route

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -1,15 +1,58 @@
 const router = require('express').Router();
+const { Op } = require('sequelize');
+const { sequelize } = require('../util/db');
 const { User } = require('../models');
 const { isAuthenticated } = require('../util/middleware');
 
+/**
+ * supported query parameters:
+ * - search: case insensitive name and username substring search
+ * - page
+ */
 router.get('/', async (req, res) => {
-  try {
-    const users = await User.findAll();
-    return res.json(users);
+  let where = {};
 
-  } catch (error) {
-    return res.status(500).send({ error });
+  if (req.query.search) {
+    where = {
+      [Op.or]: [
+        sequelize.where(
+          sequelize.fn('lower', sequelize.col('name')),
+          { [Op.substring] : req.query.search.toLowerCase() }
+        ),
+        sequelize.where(
+          sequelize.fn('lower', sequelize.col('username')),
+          { [Op.substring] : req.query.search.toLowerCase() }
+        )
+      ]
+    }
   }
+
+  let offset;
+  const pageSize = 3;
+  if (req.query.page) {
+    const pageNumber = Number(req.query.page);
+    if (isNaN(pageNumber) || pageNumber < 0) {
+      return res.status(400).send(
+        { message: 'invalid query parameter "page"' }
+      );
+    }
+    offset = pageSize * Number(req.query.page);
+  };
+
+  const users = await User.findAll({
+    attributes: {
+      exclude: ['passwordHash', 'updatedAt']
+    },
+    where,
+    order: [
+      ['username', 'ASC']
+    ],
+    offset: offset,
+    limit: pageSize,
+  });
+
+  return res.json(users);
+
 });
 
 router.get('/welcome', isAuthenticated, async (req, res) => {
