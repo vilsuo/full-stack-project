358b74b2beda3401569f9a69121374ddd9af4182
Ville Suokas <vilsuo1@gmail.com>
2023-11-15 20:39:57 +0200

    added loading text

--- frontend/src/routes/SearchPage.jsx
+++ frontend/src/routes/SearchPage.jsx
@@ -5,24 +5,42 @@ import usersService from '../services/users';
 /*
 TODO
 - show loader
+- add also name and created date (age?) to users
+- implement with mui
+- add links to user pages
 */
 const SearchPage = () => {
   const [searchParams] = useSearchParams();
   const query = searchParams.get('search');
 
   const [users, setUsers] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetch = async () => {
-      const data = await usersService.getUsers(query);
-      setUsers(data);
+      if (query) {
+        setIsLoading(true);
+
+        const data = await usersService.getUsers(query);
+        setUsers(data);
+
+        setIsLoading(false);
+      }
     };
 
     fetch();
   }, [query]);
 
+  if (isLoading) {
+    return (
+      <>
+        loading...
+      </>
+    );
+  }
+
   if (users === null) {
-    return null;
+    return <h1>give a search param</h1>;
   }
 
   return (
