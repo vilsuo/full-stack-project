6f43792e8aedb5afe9f807896b67f343cf9770c3
Ville Suokas <vilsuo1@gmail.com>
2023-11-24 11:45:32 +0200

    added test for getting single user. Also 'findUser' middleware no longer returns the passwordHash

--- backend/tests/controllers/users.test.js
+++ backend/tests/controllers/users.test.js
@@ -73,43 +73,83 @@ beforeEach(async () => {
   });
 });
 
-describe('find users', () => {
-  test('without query, all non disabled users are returned', async () => {
-    const response = await api
-      .get(`${baseUrl}`)
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
-
-    expect(response.body).toHaveLength(2);
-
-    const usernames = response.body.map(user => user.username);
-    expect(usernames).toContain(credentials1.username);
-    expect(usernames).toContain(credentials2.username);
-    // disabled user is not returned
-    expect(usernames).not.toContain(disabledCredentials.username);
+describe('get users', () => {
+  describe('all', () => {
+    test('without query, all non disabled users are returned', async () => {
+      const response = await api
+        .get(`${baseUrl}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
+  
+      expect(response.body).toHaveLength(2);
+  
+      const usernames = response.body.map(user => user.username);
+      expect(usernames).toContain(credentials1.username);
+      expect(usernames).toContain(credentials2.username);
+      // disabled user is not returned
+      expect(usernames).not.toContain(disabledCredentials.username);
+    });
+  
+    test('with query, a subset of users is returned', async () => {
+      const searchParam = 'vil';
+      const response = await api
+        .get(`${baseUrl}/?search=${searchParam}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
+  
+      expect(response.body).toHaveLength(1);
+  
+      const usernames = response.body.map(user => user.username);
+      expect(usernames).toContain(credentials1.username);
+    });
+  
+    test('password hashes are not returned', async () => {
+      const response = await api
+        .get(`${baseUrl}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
+  
+      const returnedUser = response.body[0];
+      expect(returnedUser.passwordHash).toBeUndefined();
+    });
   });
 
-  test('with query, a subset of users is returned', async () => {
-    const searchParam = 'vil';
-    const response = await api
-      .get(`${baseUrl}/?search=${searchParam}`)
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
+  describe('single', () => {
+    test('can access existing user', async () => {
+      const response = await api
+        .get(`${baseUrl}/${credentials1.username}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
 
-    expect(response.body).toHaveLength(1);
+      expect(response.body.username).toBe(credentials1.username);
+    });
 
-    const usernames = response.body.map(user => user.username);
-    expect(usernames).toContain(credentials1.username);
-  });
+    test('can not access disabled user', async () => {
+      const response = await api
+        .get(`${baseUrl}/${disabledCredentials.username}`)
+        .expect(400)
+        .expect('Content-Type', /application\/json/);
 
-  test('password hashes are not returned', async () => {
-    const response = await api
-      .get(`${baseUrl}`)
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
+      expect(response.body.message).toBe('user is disabled');
+    });
 
-    const returnedUser = response.body[0];
-    expect(returnedUser.passwordHash).toBeUndefined();
+    test('can not access nonexisting user', async () => {
+      const response = await api
+        .get(`${baseUrl}/${nonExistingUsername}`)
+        .expect(404)
+        .expect('Content-Type', /application\/json/);
+
+      expect(response.body.message).toBe('user does not exist');
+    });
+
+    test('password hashes are not returned', async () => {
+      const response = await api
+        .get(`${baseUrl}/${credentials2.username}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
+  
+      expect(response.body.passwordHash).toBeUndefined();
+    });
   });
 });
 
