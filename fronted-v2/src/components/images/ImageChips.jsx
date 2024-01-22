79d6d2563268a32ce2057c858594554556189f53
Ville Suokas <vilsuo1@gmail.com>
2024-01-22 23:07:57 +0200

    chip now handles the new editedAt field of the image model instead of the old updatedAt

--- fronted-v2/src/components/images/ImageChips.jsx
+++ fronted-v2/src/components/images/ImageChips.jsx
@@ -2,14 +2,14 @@ import util from '../../util';
 
 const ImageChips = ({ image, user }) => {
   const { username } = user;
-  const { privacy, createdAt, updatedAt } = image;
+  const { privacy, createdAt, editedAt } = image;
 
   return (
     <div className='chips'>
       <div className='chip'>Image by {username}</div>
       <div className='chip'>Created {util.formatDate(createdAt)}</div>
-      { (createdAt !== updatedAt) && 
-        <div className='chip'>Last edited {util.formatDate(updatedAt)}</div>
+      { editedAt && 
+        <div className='chip'>Last edited {util.formatDate(editedAt)}</div>
       }
       <div className='chip'>{privacy}</div>
     </div>
