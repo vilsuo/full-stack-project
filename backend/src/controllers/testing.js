796004e6d32ba97bdbc21eb369a1c11c740b0037
Ville Suokas <vilsuo1@gmail.com>
2024-01-26 17:30:38 +0200

    added a test environment only route to create disabled users

--- backend/src/controllers/testing.js
+++ backend/src/controllers/testing.js
@@ -1,11 +1,32 @@
 const testingRouter  = require('express').Router();
+const { User } = require('../models');
 const { sequelize } = require('../util/db');
+const { getNonSensitiveUser } = require('../util/dto');
 
 testingRouter.post('/reset', async (req, res) => {
   // create the tables, dropping them first if they already existed
   await sequelize.sync({ force: true });
 
   return res.status(204).end();
+});
+
+/**
+ * Route to create disabled users
+ */
+testingRouter.post('/disabled', async (req, res) => {
+  const { name, username, password } = req.body;
+
+  const user = await User.create({
+    name, username,
+
+    // beforeCreate hook encodes password so the password can be validated
+    passwordHash: password,
+
+    // create diabled user
+    disabled: true,
+  });
+
+  return res.status(201).send(getNonSensitiveUser(user));
 });
 
 module.exports = testingRouter;
